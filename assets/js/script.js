bfa38b878229747e28b83598bf0015d36f7e45eb
divansh-529 <divanshsachdev@gmail.com>
2024-12-04 15:29:12 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        const preloader = document.getElementById("preloader");
+        const content = document.getElementById("content");
+        preloader.style.display = "none";
+        content.style.display = "bloc";
+    }, 4000);
+});
+
+
+
+
+const headers = document.querySelectorAll('.accordion-header');
+headers.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        const icon = header.querySelector('.icon');
+
+        if (content.classList.contains('open')) {
+            content.classList.remove('open');
+            icon.textContent = '+';
+        } else {
+            document.querySelectorAll('.accordion-content').forEach(c => c.classList.remove('open'));
+            document.querySelectorAll('.accordion-header .icon').forEach(i => i.textContent = '+');
+            content.classList.add('open');
+            icon.textContent = '-';
+        }
+    });
+});
