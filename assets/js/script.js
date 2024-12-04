f18632968036689395d8096e5a8c24f59daeabf4
divansh-529 <divanshsachdev@gmail.com>
2024-12-04 15:52:21 +0530

    added preloder

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const content = document.getElementById("content");
         preloader.style.display = "none";
         content.style.display = "bloc";
-    }, 4000);
+    }, 1000);
 });
 
 
